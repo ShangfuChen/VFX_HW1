a8467c963b72b99f64551d23c9745a42eae88dc0
Sam Chen <b02901030@ntu.edu.tw>
2016-03-16 22:41:19 +0800

    show HDR image.

--- gsolve.m
+++ gsolve.m
@@ -7,7 +7,7 @@ k = 1;
 for i=1:size(Z,1)
    for j=1:size(Z,2)
        wij = w(Z(i,j)+1);
-       A(k,Z(i,j)+1) = wij; A(k,n+i) = -wij; b(k,1) = wij * B(i,j);
+       A(k,Z(i,j)+1) = wij; A(k,n+i) = -wij; b(k,1) = wij * B(j);
        k=k+1;
    end
 end
@@ -16,12 +16,11 @@ A(k,129) = 1;
 %% Include the smoothness equations
 k=k+1;
 for i=1:n-2
- A(k,i)=l*w(i+1); A(k,i+1)=-2*l*w(i+1); A(k,i+2)=l*w(i+1);
- k=k+1;
+    A(k,i)=l*w(i+1); A(k,i+1)=-2*l*w(i+1); A(k,i+2)=l*w(i+1);
+    k=k+1;
 end
 %%solve the matrix equations Ax = b
 X = A\b;
 g = X(1:256,1);
-lE= X(257:n+size(Z,1),1);
+lE = X(257:n+size(Z,1),1);
 end
-
--- hdr.m
+++ hdr.m
@@ -4,8 +4,9 @@ width = 1024;
 numPics = 13;
 zmin = 1;
 zmax = 256;
-%img = [ width, height, 3, 13];
 imgCell = cell( numPics, 1 );
+t = cputime;
+%img = [ width, height, 3, 13];
 
 for i=1:numPics;
  s1 = 'ex_pic/img';
@@ -21,18 +22,20 @@ end
 % create the image array X(pixel, images)
 numPixels = 50;
 for i=1:numPixels;
-    x = randi(width,1);
-    y = randi(height,1);
+    x = randi(width);
+    y = randi(height);
     for j=1:numPics;
-     img = imgCell{j};
-     Z(i,j) = img( y, x, 1);
+        img = imgCell{j};
+        Zr(i,j) = img(y,x,1);
+        Zg(i,j) = img(y,x,2);
+        Zb(i,j) = img(y,x,3);
     end
 end
 %Array of shutter speed
-B = zeros(numPixels,numPics);
-B(:,1)=log(13); B(:,2)=log(10); B(:,3)=log(4); B(:,4)=log(3.2); B(:,5)=log(1);
-B(:,6)=log(0.8); B(:,7)=log(0.3); B(:,8)=log(1/4); B(:,9)=log(1/60); B(:,10)=log(1/80);
-B(:,11)=log(1/320); B(:,12)=log(1/400); B(:,13)=log(1/1000);
+B = zeros(numPics,1);
+B(1)=log(13); B(2)=log(10); B(3)=log(4); B(4)=log(3.2); B(5)=log(1);
+B(6)=log(0.8); B(7)=log(0.3); B(8)=log(1/4); B(9)=log(1/60); B(10)=log(1/80);
+B(11)=log(1/320); B(12)=log(1/400); B(13)=log(1/1000);
 
 %[ 13, 10, 4, 3.2, 1, 0.8, 0.3, 1/4, 1/60, 1/80, 1/320, 1/400, 1/1000];
 
@@ -43,8 +46,34 @@ w = zeros(1,256);
 w(1:128) = (1:128);
 w(129:256)=(128:-1:1);
 %call functions to solce g, lE
-[g, lE] = gsolve(Z,B,l,w);
-plot(g)
+[gr, lEr] = gsolve(Zr,B,l,w);
+[gg, lEg] = gsolve(Zg,B,l,w);
+[gb, lEb] = gsolve(Zb,B,l,w);
+
+hdrImg = zeros(height,width);
+for i = 1:height;
+    for j = 1:width;
+        wij = 0;
+        lEg = 0;
+        for k = 1:numPics;
+            lE = gg(imgCell{k}(i,j,2)+1) - B(k);
+            lEg = w(imgCell{k}(i,j,2)+1)*lE + lEg;
+            wij = wij + w(imgCell{k}(i,j,2)+1); 
+        end
+        lEg = lEg/wij;
+        hdrImg(i,j) = lEg;
+    end
+end
+imshow(hdrImg)
+colormap (jet)
+caxis auto
+timeCost = cputime - t;
+% plot(gr,'r')
+% figure
+% plot(gg,'g')
+% figure
+% plot(gb,'b')
+
 %img = imread(s);
 %size(img);
 %imshow(img);
