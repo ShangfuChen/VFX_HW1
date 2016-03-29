a935d9dbe325c6b8d6449e555cdde4ee0e86c1a1
Eric Jau <ericjau@EricdeMacBook-Pro-3.local>
2016-03-29 19:40:45 +0800

    0328

--- hdr.m
+++ hdr.m
@@ -1,26 +1,47 @@
 %% Input image
-height = 768;
-width = 1024;
-numPics = 13;
+clear
+%height = 3456;
+%width = 5184;
+%input variables
+numPics = 10;
+file_name = 'pic_ex';
 zmin = 1;
 zmax = 256;
 imgCell = cell( numPics, 1 );
 t = cputime;
+B = zeros(numPics,1);
 %img = [ width, height, 3, 13];
+%count time comsuming
+t = cputime;
 
 for i=1:numPics;
- s1 = 'ex_pic/img';
+ s1 = '/img';
  s2 = [ int2str(i) ];
  if( i < 10 )
      s2 = [ '0' s2 ];
  end
  s3 = '.jpg';
- s = [ s1 s2 s3 ];
+ s = [ file_name s1 s2 s3 ];
  imgCell{i} = imread(s);
+ info = imfinfo(s);
+ B(i) = info.DigitalCamera.ExposureTime;
 end
+
+%% alignment
+iterator = 5;
+for time = 1:2;
+    for i=2:numPics;
+     [ imgCell{1} , imgCell{i} ] = imgalign(imgCell{1}, imgCell{i}, iterator );
+    end
+end
+
 
 %% calculate g function
 % create the image array X(pixel, images)
+imgSize = size(imgCell{1}(:,:,1));
+height = imgSize(1);
+width = imgSize(2);
+
 numPixels = 50;
 for i=1:numPixels;
     x = randi(width);
@@ -33,10 +54,10 @@ for i=1:numPixels;
     end
 end
 %Array of shutter speed
-B = zeros(numPics,1);
-B(1)=log(13); B(2)=log(10); B(3)=log(4); B(4)=log(3.2); B(5)=log(1);
-B(6)=log(0.8); B(7)=log(0.3); B(8)=log(1/4); B(9)=log(1/60); B(10)=log(1/80);
-B(11)=log(1/320); B(12)=log(1/400); B(13)=log(1/1000);
+B = log(B);
+%B(1)=log(13); B(2)=log(10); B(3)=log(4); B(4)=log(3.2); B(5)=log(1);
+%B(6)=log(0.8); B(7)=log(0.3); B(8)=log(1/4); B(9)=log(1/60); B(10)=log(1/80);
+%B(11)=log(1/320); B(12)=log(1/400); B(13)=log(1/1000);
 
 %[ 13, 10, 4, 3.2, 1, 0.8, 0.3, 1/4, 1/60, 1/80, 1/320, 1/400, 1/1000];
 
@@ -50,6 +71,9 @@ w(129:256)=(128:-1:1);
 [gr, lEr] = gsolve(Zr,B,l,w);
 [gg, lEg] = gsolve(Zg,B,l,w);
 [gb, lEb] = gsolve(Zb,B,l,w);
+
+solved = 'G function get'
+
 gcell = cell(3,1);
 gcell{1} = gr;
 gcell{2} = gg;
@@ -63,41 +87,79 @@ for c = 1:3;
             wij = 0;
             lEg = 0;
             for k = 1:numPics;
-                lE = gcell{c}(imgCell{k}(i,j,2)+1) - B(k);
-                lEg = w(imgCell{k}(i,j,2)+1)*lE + lEg;
-                wij = wij + w(imgCell{k}(i,j,2)+1); 
+                lE = gcell{c}(imgCell{k}(i,j,c)+1) - B(k);
+                lEg = w(imgCell{k}(i,j,c)+1)*lE + lEg;
+                wij = wij + w(imgCell{k}(i,j,c)+1); 
             end
             lEg = lEg/wij;
             hdrImg(i,j,c) = exp(lEg);
+            %hdrImg(i,j,c) = lEg;
         end
     end
 end
-minPix = min(min(min(hdrImg)));
-hdrImg = hdrImg/minPix;
+
+'finish the hdr image'
+
+%% do tone mappping by matlab
+    maxPix = max(max(max(hdrImg(:,:,1))));
+    hdrImg2 = hdrImg/maxPix;
+
+rgbImg = tonemap(hdrImg2);
+figure;
+imshow(rgbImg)
+
+%hdrIm = hdrImg2(:,:,1);
+%figure;
+%imshow(hdrIm)
 
 
-imshow(hdrImg)
+
+%% print out the map
+
+hdrIm = hdrImg(:,:,1);
+
+figure
 colormap (jet)
 caxis auto
-timeCost = cputime - t;
-%max(max(hdrImg))
-%min(min(hdrImg))
-%% Tone mapping
-for c = 1:3;
-    hdr = hdrImg(:,:,c);
-    mapImg(:,:,c) = toneMapping(hdr);
-end
-%max(max(mapImg))
-%min(min(mapImg))
-mapImg = round(mapImg*256);
-mapImg = uint8(mapImg);
-hdr = mapImg(:,:,2);
-imshow(hdr)
-imshow(mapImg)
-imwrite(mapImg, 'hdrImg.bmp');
+imshow(hdrImg)
 
+%% other's tonemapping
+%fc_tonemap(hdrImg);
+
+
+%% Tone mapping 
+
+% Normalization
+% trying how to normalize
+minP = min(min(hdrImg));
+for c=1:3
+    minPix = min(min(hdrImg(:,:,c)))
+    %maxPix = max(max(hdrImg(:,:,c)))
+    %hdrImg(:,:,c) = hdrImg(:,:,c)/minPix;
+    hdrImg(:,:,c) = hdrImg(:,:,c)/min(minP);
+end
+
+%%
+%throw into tone mapping function
+
+lightness = [ 0.01, 0.015, 0.02 ]; 
+for num = 1 : size(lightness,2);
+    mapImg = zeros(height, width, 3);
+    for c = 1:3;
+        hdrI = hdrImg(:,:,c);
+        mapImg(:,:,c) = toneMapping(hdrI,lightness(num));
+        %mapImg(:,:,c) = toneMapping2(hdrI);
+    end
+    max(max(mapImg))
+    min(min(mapImg))
+    mapImg = round(mapImg*256);
+    mapImg = uint8(mapImg);
+    figure;
+    imshow(mapImg)
+    output_name = [ file_name num2str(lightness(num)) '_hdrImg.bmp' ];
+    imwrite(mapImg, output_name);
+end
 %colormap (jet)
-caxis auto
 % plot(gr,'r')
 % figure
 % plot(gg,'g')
@@ -108,3 +170,7 @@ caxis auto
 %size(img);
 %imshow(img);
 
+%% show non-linear curve
+figure
+plot(gg);
+time_cost = cputime - t;
